a45dd9096dc5b3b8b5e611dd7b4e521a4e40ded9
eucli <eucli@EUCLIDESMARQUES>
2024-12-16 19:38:53 -0300

    Adicionar arquivos de projeto.

---
+++ LanchesMac/Areas/Admin/Servicos/RelatorioLanchesService.cs
@@ -0,0 +1,36 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Areas.Admin.Servicos
+{
+    public class RelatorioLanchesService
+    {
+        private readonly AppDbContext _context;
+
+        public RelatorioLanchesService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Lanche>> GetLanchesReport()
+        {
+            var lanches = await _context.Lanches.ToListAsync();
+
+            if(lanches is  null)
+                return default(IEnumerable<Lanche>);
+
+            return lanches;
+        }
+
+        public async Task<IEnumerable<Categoria>> GetCategoriasReport()
+        {
+            var categoria = await _context.Categorias.ToListAsync();
+
+            if (categoria is null)
+                return default(IEnumerable<Categoria>);
+
+            return categoria;
+        }
+    }
+}
